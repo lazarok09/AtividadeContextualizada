ec612cf2e511543428fec026bb16f8cb3f41b7cf
lazarok09 <lazarok09@gmail.com>
2021-02-02 10:12:49 -0300

    hotfix(validarcpf) new rule to validate cpf

--- src/atividadecontextualizada/ValidarCpf.java
+++ src/atividadecontextualizada/ValidarCpf.java
@@ -1,30 +1,29 @@
 package atividadecontextualizada;
+
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class ValidarCpf {
-      String cpf;
-	public String testarCpf(String cpf){
+	String cpf;
+
+	public String testarCpf(String cpf) {
 		this.cpf = cpf;
-		if(cpf.equals(" ")){
+		if (cpf.equals(" ")) {
 			System.out.println("O campo não pode ficar em branco!");
-                        return "erro";
-		}
-		else{
+			return "erro";
+		} else {
 			Pattern pattern = Pattern.compile("[a*z]");
 			Matcher matcher = pattern.matcher(cpf);
-			if(matcher.find()){
+			if (matcher.find()) {
 				System.out.println("CPF Não deve conter letras!");
-                                return "erro";
+				return "erro";
 			}
-                        if(cpf.length() < 11) {
-                            System.out.println("CPF deve conter 11 números");
-                            return "erro";
-                        }
+			if (cpf.length() < 11 || cpf.length() > 11) {
+				System.out.println("CPF deve conter 11 números");
+				return "erro";
+			}
 
 		}
 		return cpf;
 	}
 }
-
-

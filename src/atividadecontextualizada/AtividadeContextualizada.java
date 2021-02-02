1458039fd98d8615b2782c414a956a4d090e5ab0
lazarok09 <lazarok09@gmail.com>
2021-02-02 17:36:52 -0300

    :pencil: doc(description) remove some parts

--- src/atividadecontextualizada/AtividadeContextualizada.java
+++ src/atividadecontextualizada/AtividadeContextualizada.java
@@ -4,7 +4,7 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 /*
-* @Autor Lazaro Jose da Silva de Souza - UNINABUCO 2021
+* @Autor Lazaro Souza - UNINABUCO 2021
 * github: https://github.com/lazarok09
 *
 */
